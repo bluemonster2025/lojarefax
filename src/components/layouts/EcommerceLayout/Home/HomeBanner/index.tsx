0ffeb717f88ffe1357525ab8cef712d5ad101c56
cheillaamelo <cheillaaaa@gmail.com>
2025-10-31 10:29:47 -0300

    fix component HomeBanner

--- src/components/layouts/EcommerceLayout/Home/HomeBanner/index.tsx
+++ src/components/layouts/EcommerceLayout/Home/HomeBanner/index.tsx
@@ -1,6 +1,4 @@
-import BuyButton from "@/components/elements/BuyButton";
 import { Section } from "@/components/elements/Section";
-import { Title } from "@/components/elements/Texts";
 
 interface BannerProps {
   imgUrlMobile?: string;
@@ -16,37 +14,11 @@ export default function HomeBanner({
   return (
     <>
       {/* DESKTOP */}
-      <Section className="hidden md:block pb-12 md:pb-14">
+      <Section className="hidden md:block pb-12 md:pb-14 cursor-pointer">
         <div
           className="relative w-full md:aspect-[1296/413] bg-cover bg-no-repeat bg-center md:rounded-3xl overflow-hidden"
           style={{ backgroundImage: `url(${imgUrlDesktop})` }}
-        >
-          {/* Texto e botão sobre a imagem */}
-          <div className="gap-8 absolute inset-0 flex flex-col items-start justify-center px-12 text-white z-10">
-            <div className="gap-4">
-              <Title as={"h4"} variant="h4">
-                Produtos sob demanda
-              </Title>
-              <Title as="h2" variant="hero" className="max-w-[327px]">
-                monte seu projeto
-              </Title>
-              <Title as={"h4"} variant="h4" className="max-w-[327px]">
-                solicite um orçamento exclusivo para o seu projeto
-              </Title>
-            </div>
-
-            {/* Botão Comprar */}
-            <div className="w-fit">
-              <BuyButton
-                title="Envie seu projeto para orçamento"
-                icon="BsWhatsapp"
-              />
-            </div>
-          </div>
-
-          {/* Overlay opcional para melhor contraste */}
-          <div className="absolute inset-0 bg-black/30 z-0"></div>
-        </div>
+        ></div>
       </Section>
 
       {/* MOBILE */}
@@ -54,17 +26,7 @@ export default function HomeBanner({
         <div
           className="relative w-full aspect-[1.44/1] bg-cover bg-no-repeat bg-center overflow-hidden"
           style={{ backgroundImage: `url(${imgUrlMobile})` }}
-        >
-          <div className="absolute inset-0 flex flex-col items-start justify-center px-6 text-white z-10">
-            <Title as="h3" variant="h3" className="mb-3">
-              Monte
-            </Title>
-            <button className="bg-white text-black px-5 py-2 rounded-full font-semibold hover:bg-gray-100 transition">
-              Button
-            </button>
-          </div>
-          <div className="absolute inset-0 bg-black/30 z-0"></div>
-        </div>
+        ></div>
       </div>
     </>
   );
